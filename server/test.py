ae59237b8b84846f4dbde939f3c712dbea62fdb8
Caio Gevegir <56521026+caiogevegir@users.noreply.github.com>
2024-01-27 20:23:21 -0300

    Initial commit

---
+++ server/test.py
@@ -0,0 +1,6 @@
+from main import update_game, remove_game, Game
+
+game = Game(name='Axiom Verge', platform='SWITCH', status='PLAYING', score=9)
+
+#update_game('d517f3f5-2b50-46b7-ab44-138b7f072fed', game)
+remove_game('d517f3f5-2b50-46b7-ab44-138b7f072fed')
